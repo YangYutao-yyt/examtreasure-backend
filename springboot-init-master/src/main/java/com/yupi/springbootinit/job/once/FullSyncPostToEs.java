f551316efd360ae9c964f57112b43e4565bf7fd3
杨宇涛 <2794093098@qq.com>
2025-03-26 23:19:06 +0800

    project init

---
+++ springboot-init-master/src/main/java/com/yupi/springbootinit/job/once/FullSyncPostToEs.java
@@ -0,0 +1,48 @@
+package com.yyt.examtreasure.job.once;
+
+import com.yyt.examtreasure.esdao.PostEsDao;
+import com.yyt.examtreasure.model.dto.post.PostEsDTO;
+import com.yyt.examtreasure.model.entity.Post;
+import com.yyt.examtreasure.service.PostService;
+import java.util.List;
+import java.util.stream.Collectors;
+import javax.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import cn.hutool.core.collection.CollUtil;
+import org.springframework.boot.CommandLineRunner;
+
+/**
+ * 全量同步帖子到 es
+ *
+ * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
+ * @from <a href="https://yupi.icu">编程导航知识星球</a>
+ */
+// todo 取消注释开启任务
+//@Component
+@Slf4j
+public class FullSyncPostToEs implements CommandLineRunner {
+
+    @Resource
+    private PostService postService;
+
+    @Resource
+    private PostEsDao postEsDao;
+
+    @Override
+    public void run(String... args) {
+        List<Post> postList = postService.list();
+        if (CollUtil.isEmpty(postList)) {
+            return;
+        }
+        List<PostEsDTO> postEsDTOList = postList.stream().map(PostEsDTO::objToDto).collect(Collectors.toList());
+        final int pageSize = 500;
+        int total = postEsDTOList.size();
+        log.info("FullSyncPostToEs start, total {}", total);
+        for (int i = 0; i < total; i += pageSize) {
+            int end = Math.min(i + pageSize, total);
+            log.info("sync from {} to {}", i, end);
+            postEsDao.saveAll(postEsDTOList.subList(i, end));
+        }
+        log.info("FullSyncPostToEs end, total {}", total);
+    }
+}
---
+++ src/main/java/com/yyt/examtreasure/mapper/PostThumbMapper.java
@@ -0,0 +1,16 @@
+package com.yyt.examtreasure.mapper;
+
+import com.yyt.examtreasure.model.entity.PostThumb;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 帖子点赞数据库操作
+ *
+ */
+public interface PostThumbMapper extends BaseMapper<PostThumb> {
+
+}
+
+
+
+
