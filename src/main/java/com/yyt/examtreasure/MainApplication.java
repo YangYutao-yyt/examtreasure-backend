805e301e74aa63bd221c7e0ae8e9ed5ba54a2e2b
杨宇涛 <2794093098@qq.com>
2025-04-02 23:54:52 +0800

    Add, delete, modify and check

--- src/main/java/com/yyt/examtreasure/MainApplication.java
+++ src/main/java/com/yyt/examtreasure/MainApplication.java
@@ -13,7 +13,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  */
 // todo 如需开启 Redis，须移除 exclude 中的内容
 @SpringBootApplication(exclude = {RedisAutoConfiguration.class})
-@MapperScan("com.yupi.springbootinit.mapper")
+@MapperScan("com.yyt.examtreasure.mapper")
 @EnableScheduling
 @EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
 public class MainApplication {
