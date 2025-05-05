8b2ab7f85ef79d7e06e3fb27c5a30191603bcd8c
杨宇涛 <2794093098@qq.com>
2025-05-06 00:17:37 +0800

    分级题目反爬虫策略

--- src/main/java/com/yyt/examtreasure/controller/QuestionController.java
+++ src/main/java/com/yyt/examtreasure/controller/QuestionController.java
@@ -1,6 +1,7 @@
 package com.yyt.examtreasure.controller;
 
 import cn.dev33.satoken.annotation.SaCheckRole;
+import cn.dev33.satoken.stp.StpUtil;
 import cn.hutool.core.collection.CollUtil;
 import cn.hutool.json.JSONUtil;
 import com.alibaba.csp.sentinel.Entry;
@@ -21,6 +22,7 @@ import com.yyt.examtreasure.common.ResultUtils;
 import com.yyt.examtreasure.constant.UserConstant;
 import com.yyt.examtreasure.exception.BusinessException;
 import com.yyt.examtreasure.exception.ThrowUtils;
+import com.yyt.examtreasure.manager.CounterManager;
 import com.yyt.examtreasure.model.dto.question.*;
 import com.yyt.examtreasure.model.entity.Question;
 import com.yyt.examtreasure.model.entity.QuestionBankQuestion;
@@ -36,6 +38,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 题目接口
@@ -155,12 +158,55 @@ public class QuestionController {
     @GetMapping("/get/vo")
     public BaseResponse<QuestionVO> getQuestionVOById(long id, HttpServletRequest request) {
         ThrowUtils.throwIf(id <= 0, ErrorCode.PARAMS_ERROR);
+        // 检测和处置爬虫（可以自行扩展为 - 登录后才能获取到答案）
+        User loginUser = userService.getLoginUserPermitNull(request);
+        if (loginUser != null) {
+            crawlerDetect(loginUser.getId());
+        }
+        // 友情提示，对于敏感的内容，可以再打印一些日志，记录用户访问的内容
         // 查询数据库
         Question question = questionService.getById(id);
         ThrowUtils.throwIf(question == null, ErrorCode.NOT_FOUND_ERROR);
         // 获取封装类
         return ResultUtils.success(questionService.getQuestionVO(question, request));
     }
+
+    // 仅是为了方便，才把这段代码写到这里
+    @Resource
+    private CounterManager counterManager;
+
+    /**
+     * 检测爬虫
+     *
+     * @param loginUserId
+     */
+    private void crawlerDetect(long loginUserId) {
+        // 调用多少次时告警
+        final int WARN_COUNT = 10;
+        // 调用多少次时封号
+        final int BAN_COUNT = 20;
+        // 拼接访问 key
+        String key = String.format("user:access:%s", loginUserId);
+        // 统计一分钟内访问次数，180 秒过期
+        long count = counterManager.incrAndGetCounter(key, 1, TimeUnit.MINUTES, 180);
+        // 是否封号
+        if (count > BAN_COUNT) {
+            // 踢下线
+            StpUtil.kickout(loginUserId);
+            // 封号
+            User updateUser = new User();
+            updateUser.setId(loginUserId);
+            updateUser.setUserRole("ban");
+            userService.updateById(updateUser);
+            throw new BusinessException(ErrorCode.NO_AUTH_ERROR, "访问次数过多，已被封号");
+        }
+        // 是否告警
+        if (count == WARN_COUNT) {
+            // 可以改为向管理员发送邮件通知
+            throw new BusinessException(110, "警告：访问太频繁");
+        }
+    }
+
 
     /**
      * 分页获取题目列表（仅管理员可用）
